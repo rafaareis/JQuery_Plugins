efd882673d59b75450e847242c3c6fd6f64d8aef
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-04-07 19:37:26 -0300

    Commit 07042023-1937 - Aulas 1 e 2

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburger').click(function () {
+        $('nav').slideToggle();
+    })
+
+})
