cdc750caf26a17f915a729775a2ea882ada15eb8
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-04-10 11:44:41 -0300

    Commit 10042023-1142: Adicionando a Aula 04

--- main.js
+++ main.js
@@ -7,4 +7,43 @@ $(document).ready(function() {
         $('nav').slideToggle();
     })
 
+    $('#telefone').mask('+00(00)00000-0000', {
+        placeholder:'+__(__)_____-____'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true,
+            },
+            veiculo: {
+                required: false,
+            }
+        },
+
+        messages: {
+            nome: 'Por favor, insira o seu nome.'
+        },
+
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
 })
